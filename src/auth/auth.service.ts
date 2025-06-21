c8e7694c32909eae7a238fa51690611dc2c3d4d7
ad-ng <adolphengoga@gmail.com>
2025-06-21 19:51:20 +0200

    logging in user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as argon from 'argon2';
+import { loginDTO } from './dtos';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
+
+  async signin(dto: loginDTO) {
+    const { email, password } = dto;
+
+    const currentUser = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!currentUser) {
+      throw new ForbiddenException('invalid credentials');
+    }
+
+    const checkPassword: boolean = await argon.verify(
+      currentUser.password,
+      password,
+    );
+
+    if (!checkPassword) {
+      throw new ForbiddenException('invalid credentials');
+    }
+
+    return {
+      token: await this.jwt.signAsync(currentUser),
+      data: currentUser,
+    };
+  }
+}
