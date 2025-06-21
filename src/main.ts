5677c65a933fc246d28d291059574e42acb1cd05
ad-ng <adolphengoga@gmail.com>
2025-06-21 17:40:40 +0200

    initialising swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Property Management System')
+    .setDescription(
+      'Near Me is a location-based backend API that helps travelers and locals discover nearby services, attractions, and businesses. It offers features like location search, service filtering, user reviews, and real-time suggestions—similar to Google Maps or TripAdvisor. Built for speed, accuracy, and scalability.',
+    )
+    .setVersion('1.0')
+    .setContact(
+      'NGOGA Adolphe',
+      'https://github.com/ad-ng',
+      'adolphengoga@gmail.com',
+    )
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, documentFactory);
+
   app.use(helmet());
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
