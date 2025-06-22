b1c838ff46df9a3361ee0bc9ee25c677886ccf94
ad-ng <adolphengoga@gmail.com>
2025-06-22 05:48:38 +0200

    setting login dto for swagger

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class loginDTO {
+  @ApiProperty({ example: 'johndoe@gmail.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'test@123' })
   @IsString()
   @IsNotEmpty()
   password: string;
