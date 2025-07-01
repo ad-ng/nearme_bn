70f8f1be9851a40b87a4b7b23b3371b95be94901
ad-ng <adolphengoga@gmail.com>
2025-07-01 15:48:27 +0200

    updating country

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard, RolesGuard } from 'src/auth/guards';
 import { UserService } from './user.service';
 import { Request } from 'express';
-import { NamesDto } from './dtos';
+import { CountryDTO, NamesDto } from './dtos';
 
 @UseGuards(AuthGuard, RolesGuard)
 @Controller('user')
@@ -17,5 +17,10 @@ export class UserController {
   @Patch('names')
   updateNames(@Req() req: Request, @Body() dto: NamesDto) {
     return this.userService.updateNames(dto, req.user);
+  }
+
+  @Patch('country')
+  updateCountry(@Req() req: Request, @Body() dto: CountryDTO) {
+    return this.userService.updateCountry(dto, req.user);
   }
 }
