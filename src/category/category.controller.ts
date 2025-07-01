d894d90083590706cacfc9f57ed4c382d5f4aa78
ad-ng <adolphengoga@gmail.com>
2025-07-01 17:58:50 +0200

    fetching categories

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { AuthGuard, RolesGuard } from 'src/auth/guards';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@UseGuards(AuthGuard, RolesGuard)
+@ApiBearerAuth()
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Get()
+  fetchCategories() {
+    return this.categoryService.fetchAllCategories();
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async fetchAllCategories() {
+    try {
+      const allCategories = await this.prisma.category.findMany();
+
+      return {
+        message: 'categories fetched successfully',
+        data: allCategories,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
