c14e1c1d11e035c0d95f8bd7dec3d309b5cc2fb1
ad-ng <adolphengoga@gmail.com>
2025-07-09 06:55:56 +0200

    adding placeItemDTO

--- src/category/dto/index.ts
+++ src/category/dto/index.ts
@@ -1,2 +1,3 @@
 export * from './category.dto';
 export * from './subCategory.dto';
+export * from './placeItem.dto';
---
+++ src/category/dto/placeItem.dto.ts
@@ -0,0 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+export class PlaceItemDTO {
+  @ApiProperty({ example: 'Safe Ride Car' })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({ example: 'jbchbchbchbhfbhbhbchbhcbchchcccvbdbvhcbv' })
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty({ example: '8:00 AM - 9:00 PM' })
+  @IsNotEmpty()
+  @IsString()
+  workingHours: string;
+
+  @ApiProperty({ example: 'Kigali - Rwanda' })
+  @IsNotEmpty()
+  @IsString()
+  location: string;
+
+  @ApiProperty({ example: 'https://img.com' })
+  @IsNotEmpty()
+  @IsArray()
+  placeImg: string[];
+
+  @ApiProperty({ example: 'businessemail@gmail.com' })
+  @IsNotEmpty()
+  @IsString()
+  businessEmail: string;
+
+  @ApiProperty({ example: '+250787377398' })
+  @IsNotEmpty()
+  @IsString()
+  phoneNumber: string;
+
+  @ApiProperty({ example: 'Private Transport' })
+  @IsNotEmpty()
+  @IsString()
+  subCategoryName: string;
+}
