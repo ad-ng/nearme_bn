e7eafdc7d1e30cfe5f3754309539b79f4ba1b68e
ad-ng <adolphengoga@gmail.com>
2025-07-27 13:40:37 +0200

    fetchinng locations in a province

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class LocationService {}
+export class LocationService {
+  constructor(private prisma: PrismaService) {}
+
+  async fetchLocationsInProvince(provinceName: string) {
+    const checkProvince = await this.prisma.provinces.findFirst({
+      where: { name: provinceName },
+    });
+    if (!checkProvince) {
+      return new NotFoundException(`no ${provinceName} Province found`);
+    }
+
+    try {
+      const allLocations = await this.prisma.locations.findMany({
+        where: { provinceId: checkProvince.id },
+      });
+      return {
+        message: 'Locations found successfully',
+        data: allLocations,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
