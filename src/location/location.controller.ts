e9c7fd17e130f3fc70f14f570e6746eb9d60508c
ad-ng <adolphengoga@gmail.com>
2025-07-27 13:40:37 +0200

    fetchinng locations in a province

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { Controller, Get, Param } from '@nestjs/common';
+import { LocationService } from './location.service';
 
 @Controller('location')
-export class LocationController {}
+export class LocationController {
+  constructor(private locationService: LocationService) {}
+
+  @Get(':provinceName')
+  fetchLocations(@Param() Param: string) {
+    return this.locationService.fetchLocationsInProvince(Param['provinceName']);
+  }
+}
