9ba7e5b60aff10dd2f75db330138da70c4ccab64
ad-ng <adolphengoga@gmail.com>
2025-09-03 14:03:35 +0200

    saving interaction event

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AnalyticsService } from './analytics.service';
+import { AnalyticsDTO } from './dto';
+import { AuthGuard, RolesGuard } from 'src/auth/guards';
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('analytics')
-export class AnalyticsController {}
+export class AnalyticsController {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  @Post()
+  addingAnInteractionEvent(@Body() dto: AnalyticsDTO) {
+    return this.analyticsService.saveEvent(dto);
+  }
+}
--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AnalyticsDTO } from './dto';
 
 @Injectable()
-export class AnalyticsService {}
+export class AnalyticsService {
+  constructor(private prisma: PrismaService) {}
+
+  async saveEvent(dto: AnalyticsDTO) {
+    try {
+      const newRegisteredEvent = await this.prisma.interactionEvent.create({
+        data: dto,
+      });
+      return {
+        message: 'Interaction Event Saved Successfully',
+        data: newRegisteredEvent,
+      };
+    } catch (error) {
+      return new InternalServerErrorException(error);
+    }
+  }
+}
