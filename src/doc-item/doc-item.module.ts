3690e989f686a558d96bd423e3cd0477c1fbee27
ad-ng <adolphengoga@gmail.com>
2025-10-09 10:42:49 +0200

    adding images to article

--- src/doc-item/doc-item.module.ts
+++ src/doc-item/doc-item.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DocItemController } from './doc-item.controller';
 import { DocItemService } from './doc-item.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { ImagesModule } from 'src/images/images.module';
 
 @Module({
   controllers: [DocItemController],
   providers: [DocItemService],
-  imports: [AuthModule],
+  imports: [AuthModule, ImagesModule],
 })
 export class DocItemModule {}
