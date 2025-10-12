669fd4b30e6f8f80abc4d25f94e4d8f3d67e8acf
ad-ng <adolphengoga@gmail.com>
2025-10-12 08:51:31 +0200

    fixing backend

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,4 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
 import * as argon from 'argon2';
